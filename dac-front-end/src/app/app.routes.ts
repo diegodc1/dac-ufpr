9f1645e546d607e08fc98dc87b722b2e5e605586
Jacks1n <jacksmg05@gmail.com>
2025-03-26 19:42:16 -0300

    -m " Templates R03, R04, R08"

--- dac-front-end/src/app/app.routes.ts
+++ dac-front-end/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from "./components/register/register.component";
 import { LoginComponent } from './components/login/login.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { VerReservaComponent } from './components/ver-reserva/ver-reserva.component';
+import { HomeComponent } from './components/home/home.component';
 export const routes: Routes = [
   {
     path: '',
@@ -13,6 +15,20 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+  },
+
+  { path: 'header',
+    component: HeaderComponent,
+
+  },
+  {
+    path: 'ver-reserva/:codigoReserva',
+    component: VerReservaComponent,
+
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
   }
 ];
