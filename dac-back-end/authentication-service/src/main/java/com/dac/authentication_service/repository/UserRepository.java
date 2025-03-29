67313d781aae176a732285e917ded505eafa5bf3
Diego Alves <diegoalvesdc12@gmail.com>
2025-03-29 14:43:54 -0300

    ✨ :: feat(auth) :: Implementa cadastro e login de usuario

--- dac-back-end/authentication-service/src/main/java/com/dac/authentication_service/repository/UserRepository.java
+++ dac-back-end/authentication-service/src/main/java/com/dac/authentication_service/repository/UserRepository.java
@@ -2,12 +2,12 @@ package com.dac.authentication_service.repository;
 
 
 import com.dac.authentication_service.collection.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByLogin(String login);
 }
