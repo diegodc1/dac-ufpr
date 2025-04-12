0e18215b758e492b06829ddddb973aa60653e51f
aleida9 <aleida.lafuent@gmail.com>
2025-04-12 19:30:58 -0300

    Serviço de sagas

---
+++ dac-back-end/sagas/src/main/java/com/aerolinha/SagasApplication.java
@@ -0,0 +1,13 @@
+package com.aerolinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagasApplication.class, args);
+	}
+
+}
