6a9b60df3d44cdf673bb04b49ae62b47f8291678
CellyMaciel <cellymaciel005@gmail.com>
2025-04-18 20:56:48 -0300

    fix(Voss):: Arrumando retorno dos end-points

--- dac-back-end/voos/src/main/java/com/dac/voos/voos/repositorys/AeroportoRepository.java
+++ dac-back-end/voos/src/main/java/com/dac/voos/voos/repositorys/AeroportoRepository.java
@@ -4,6 +4,9 @@ import com.dac.voos.voos.entitys.Aeroporto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AeroportoRepository  extends JpaRepository<Aeroporto , String> {
+    Optional<Aeroporto> findByCodigo(String codigoAeroportoOrigem);
 }
--- dac-back-end/voos/src/main/java/com/dac/voos/voos/repositorys/VooRepository.java
+++ dac-back-end/voos/src/main/java/com/dac/voos/voos/repositorys/VooRepository.java
@@ -1,9 +1,28 @@
 package com.dac.voos.voos.repositorys;
 
+import com.dac.voos.voos.entitys.Aeroporto;
 import com.dac.voos.voos.entitys.Voos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface VooRepository extends JpaRepository<Voos, Long> {
+
+    @Query("SELECT v FROM Voos v WHERE v.data_hora BETWEEN :dataInicio AND :dataFim AND v.aeroportoOrigem = :origem AND v.aeroportoDestino = :destino")
+    List<Voos> buscarPorDataHoraOrigemDestino(
+            @Param("dataInicio") LocalDateTime dataInicio,
+            @Param("dataFim") LocalDateTime dataFim,
+            @Param("origem") Aeroporto origem,
+            @Param("destino") Aeroporto destino
+    );
+    @Query("SELECT v FROM Voos v WHERE v.data_hora BETWEEN :dataInicio AND :dataFim")
+    List<Voos> buscarVoosPorIntervaloDeDatas(
+            @Param("dataInicio") LocalDateTime dataInicio,
+            @Param("dataFim") LocalDateTime dataFim
+    );
 }
