fb1465b996bccbe19103799388e0d9e3359b6720
Diego Alves <diegoalvesdc12@gmail.com>
2025-04-18 15:22:48 -0300

    feat(RF017): Implementa modal de inserção de novo funcionário

--- dac-front-end/src/app/components/employee/list-employees/list-employees.component.ts
+++ dac-front-end/src/app/components/employee/list-employees/list-employees.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import {BoardingConfirmationModalComponent} from "../boarding-confirmation-modal/boarding-confirmation-modal.component";
 import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {HeaderEmployeeComponent} from "../header-employee/header-employee.component";
+import {FormsModule} from "@angular/forms";
+import { NgxMaskDirective,NgxMaskPipe, provideNgxMask  } from 'ngx-mask';
+import { NgForm } from '@angular/forms';
+
 
 
 interface Employee {
@@ -15,23 +19,69 @@ interface Employee {
 @Component({
   selector: 'app-list-employees',
   standalone: true,
-    imports: [
-        BoardingConfirmationModalComponent,
-        DatePipe,
-        HeaderEmployeeComponent,
-        NgForOf,
-        NgIf
-    ],
+  imports: [
+    BoardingConfirmationModalComponent,
+    DatePipe,
+    HeaderEmployeeComponent,
+    NgForOf,
+    NgIf,
+    FormsModule,
+    NgxMaskDirective,
+    NgxMaskPipe,
+  ],
   templateUrl: './list-employees.component.html',
-  styleUrl: './list-employees.component.css'
+  styleUrl: './list-employees.component.css',
+  providers: [provideNgxMask()],
 })
 export class ListEmployeesComponent {
+  showModal = false;
+  phoneMask: string = '(00) 00000-0000';
+
 
   employees: Employee[] = [
-    { id: 1, nome: 'João', cpf: '111.111.111-11', email: 'joao@gmail.com', telefone: '41 99999-9999'},
-    { id: 2, nome: 'Maria - GRU', cpf: '222.222.222-22', email: 'maria@gmail.com', telefone: '41 99999-9999'},
-    { id: 3, nome: 'Pedrinho - GRU', cpf: '333.333.333-33', email: 'pedrinho@gmail.com', telefone: '41 99999-9999' },
-    { id: 4, nome: 'Joana - GRU', cpf: '444.444.444-44U', email: 'joana@gmail.com', telefone: '41 99999-9999' }
+    { id: 1, nome: 'João', cpf: '11111111111', email: 'joao@gmail.com', telefone: '41999999999'},
+    { id: 2, nome: 'Maria - GRU', cpf: '22222222222', email: 'maria@gmail.com', telefone: '41999999999'},
+    { id: 3, nome: 'Pedrinho - GRU', cpf: '33333333333', email: 'pedrinho@gmail.com', telefone: '41999999999' },
+    { id: 4, nome: 'Joana - GRU', cpf: '44444444444U', email: 'joana@gmail.com', telefone: '41999999999' }
   ];
+
+  newEmployee: Employee = {
+    id: 0,
+    nome: '',
+    cpf: '',
+    email: '',
+    telefone: ''
+  };
+
+  addEmployee(form: NgForm): void {
+    if (form.invalid) {
+      console.log("erro")
+      return;
+    }
+
+    // Aqui você já sabe que todos os campos estão preenchidos e válidos
+    this.employees.push({ ...this.newEmployee });
+    this.newEmployee = { id: 0, nome: '', cpf: '', email: '', telefone: '' };
+    this.closeModal();
+  }
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.newEmployee = {
+      id: 0,
+      nome: '',
+      cpf: '',
+      email: '',
+      telefone: ''
+    };
+  }
 
 }
