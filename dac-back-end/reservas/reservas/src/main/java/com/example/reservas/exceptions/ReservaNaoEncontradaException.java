2bbe6ca6872f61f862b48ba49eaefd1889fe0046
Jacks1n <jacksmg05@gmail.com>
2025-05-08 15:40:57 -0300

    feat(ms-reservas)- implementar exceções

---
+++ dac-back-end/reservas/reservas/src/main/java/com/example/reservas/exceptions/ReservaNaoEncontradaException.java
@@ -0,0 +1,12 @@
+package com.example.reservas.exceptions;
+
+public class ReservaNaoEncontrado extends RuntimeException {
+
+    public ReservaNaoEncontrado(String msg) {
+        super(msg);
+    }
+
+
+}
+
+
