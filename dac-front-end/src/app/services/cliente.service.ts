39cd84111a3f640879733e8526094ca56cdda89a
Gabriel Francelino Voidaleski <gabriel.voidaleski@ufpr.br>
2025-05-15 14:35:59 -0300

    feat: serviço do cliente conectado com a API Gateway

---
+++ dac-front-end/src/app/services/cliente.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private API_URL = 'http://localhost:8091/client';
+
+  constructor(private http: HttpClient) { }
+
+  getSaldoMilhas(): Observable<number> {
+    return this.http.get<number>(`${this.API_URL}/saldo-milhas`);
+  }
+
+  getPerfilCliente(): Observable<any> {
+    return this.http.get<any>(`${this.API_URL}/perfil`);
+  }
+
+  atualizarPerfil(perfil: any): Observable<any> {
+    return this.http.put<any>(`${this.API_URL}/perfil`, perfil);
+  }
+
+  comprarMilhas(quantidade: number, valorPago: number): Observable<any> {
+    const authToken = localStorage.getItem('auth_token');
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-access-token': authToken || ''
+    });
+    
+    return this.http.post<any>(`${this.API_URL}/comprar-milhas`, 
+      { quantidade, valorPago }, 
+      { headers }
+    );
+  }
+}
