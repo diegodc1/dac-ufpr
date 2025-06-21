3e734cb1fcca79327f462f7c96635cf27796a5b9
Diego Alves <diegoalvesdc12@gmail.com>
2025-06-21 16:36:29 -0300

    feat: Adiciona mensagens de retorno ao usuário no cadastro, cancelamento e confirmação de Voo

--- dac-front-end/src/app/components/employee/home-employee/home-employee.component.ts
+++ dac-front-end/src/app/components/employee/home-employee/home-employee.component.ts
@@ -12,11 +12,12 @@ import { FlightService } from '../../../services/flight.service';
 import { Voo } from '../../../models/voo/voo.model';
 import { FuncionarioService } from '../../../services/funcionario/funcionario.service';
 import { catchError, Observable } from 'rxjs';
+import Swal from "sweetalert2";
 
 
 
 interface ModalData {
-    codigo?: number; 
+    codigo?: number;
   }
 
 @Component({
@@ -41,7 +42,7 @@ export class HomeEmployeeComponent implements OnInit {
 
   idFuncionario!: string; // esta variável armazena o id que vem do mongo db no authentication service!
   voos: Voo[] = [];
-   private baseUrl = ""; 
+   private baseUrl = "";
   http: any;
 
   constructor(
@@ -68,17 +69,17 @@ export class HomeEmployeeComponent implements OnInit {
         this.voos = [];
       }
     }
-  
+
   )
 
 
   }
-  
+
 
   modals = {
     boarding: { isOpen: false, data: null as Voo | null },
-    cancelFlight: { isOpen: false, data: null as Voo | null }, 
-    flightCompletion: { isOpen: false, data: null as Voo | null }, 
+    cancelFlight: { isOpen: false, data: null as Voo | null },
+    flightCompletion: { isOpen: false, data: null as Voo | null },
     createFlight: { isOpen: false, data: null as any }
   };
 
@@ -100,17 +101,28 @@ export class HomeEmployeeComponent implements OnInit {
 
   handleCreateFlight(flightData: any) {
     console.log('Novo voo cadastrado:', flightData);
+    Swal.fire({
+      icon: 'success',
+      title: 'Voo Cadastrado!',
+      text: `O voo foi cadastrado com sucesso!`,
+      confirmButtonColor: '#3085d6',
+    });
     this.closeModal('createFlight');
-    this.listarVoosProx48h(); 
+    this.listarVoosProx48h();
   }
 
  handleCancelFlight() {
-    const vooToCancel: Voo | null = this.modals.cancelFlight.data; 
-    if (vooToCancel && vooToCancel.codigo) { 
+    const vooToCancel: Voo | null = this.modals.cancelFlight.data;
+    if (vooToCancel && vooToCancel.codigo) {
       console.log('Solicitando cancelamento para o voo:', vooToCancel.codigo);
       this.servicoFuncionario.patchFlightState(vooToCancel.codigo, 'CANCELADO').subscribe({
         next: (updatedVoo: Voo) => {
-          console.log('Voo cancelado com sucesso:', updatedVoo);
+          Swal.fire({
+            icon: 'success',
+            title: 'Voo Cancelado!',
+            text: `Este Voo foi cancelado com sucesso!`,
+            confirmButtonColor: '#3085d6',
+          });
           const index = this.voos.findIndex(v => v.codigo === updatedVoo.codigo);
           if (index > -1) {
             this.voos[index].estado = updatedVoo.estado;
@@ -118,8 +130,12 @@ export class HomeEmployeeComponent implements OnInit {
           this.closeModal('cancelFlight');
         },
         error: (err) => {
-          console.error('Erro ao cancelar voo:', err);
-          alert(`Erro ao cancelar voo: ${err.message}`);
+          Swal.fire({
+            icon: 'error',
+            title: 'Erro!',
+            text: `Ocorreu um erro ao cancelar este voo!`,
+            confirmButtonColor: '#3085d6',
+          });
           this.closeModal('cancelFlight');
         }
       });
@@ -130,12 +146,17 @@ export class HomeEmployeeComponent implements OnInit {
   }
 
   handleFlightCompletion() {
-    const vooToComplete: Voo | null = this.modals.flightCompletion.data; 
-    if (vooToComplete && vooToComplete.codigo) { 
+    const vooToComplete: Voo | null = this.modals.flightCompletion.data;
+    if (vooToComplete && vooToComplete.codigo) {
       console.log('Solicitando realização do voo:', vooToComplete.codigo);
       this.servicoFuncionario.patchFlightState(vooToComplete.codigo, 'REALIZADO').subscribe({
         next: (updatedVoo: Voo) => {
-          console.log('Voo marcado como realizado com sucesso:', updatedVoo);
+          Swal.fire({
+            icon: 'success',
+            title: 'Voo Realizado!',
+            text: `O Voo foi marcado como realizado com sucesso!`,
+            confirmButtonColor: '#3085d6',
+          });
           const index = this.voos.findIndex(v => v.codigo === updatedVoo.codigo);
           if (index > -1) {
             this.voos[index].estado = updatedVoo.estado;
@@ -143,8 +164,12 @@ export class HomeEmployeeComponent implements OnInit {
           this.closeModal('flightCompletion');
         },
         error: (err) => {
-          console.error('Erro ao marcar voo como realizado:', err);
-          alert(`Erro ao marcar voo como realizado: ${err.message}`);
+          Swal.fire({
+            icon: 'error',
+            title: 'ERRO',
+            text: `Erro ao marcar voo como realizado! ${err.message}`,
+            confirmButtonColor: '#3085d6',
+          });
           this.closeModal('flightCompletion');
         }
       });
